9c239808b4e771e31ed0ab2a3977f591d99c3422
Niels Engelhard <engelhardniels@gmail.com>
2026-09-26 15:35:14 +0200

    just single words (smaller names)

--- src/playhaus-api/internal/user/username.go
+++ src/playhaus-api/internal/user/username.go
@@ -1,46 +1,66 @@
 package user
 
 import (
-	"fmt"
 	"math/rand/v2"
+
 	"playhaus-api/internal/i18n"
 )
 
-var enAdjectives = []string{
-	"Red", "Blue", "Green", "Orange", "Pink", "Yellow", "Gray", "Orange", "Brown", "Black",
-}
-
-var enNouns = []string{
+var enWords = []string{
 	"Banana", "Potato", "Pancake", "Goofball", "Dumpling",
 	"Cheese", "Noodle", "Meatball", "Pickle", "Shoe",
+	"Rocket", "Biscuit", "Waffle", "Muffin", "Cookie",
+	"Donut", "Pretzel", "Popcorn", "Taco", "Burrito",
+	"Penguin", "Monkey", "Giraffe", "Elephant", "Otter",
+	"Badger", "Panda", "Koala", "Hamster", "Hedgehog",
+	"Dragon", "Wizard", "Goblin", "Pirate", "Ninja",
+	"Robot", "Alien", "Monster", "Ghost", "Vampire",
+	"Thunder", "Rainbow", "Comet", "Meteor", "Sunshine",
+	"Bubble", "Rocket", "Sparkle", "Shadow", "Breeze",
+	"Button", "Bucket", "Hammer", "Pencil", "Backpack",
+	"Socks", "Slipper", "Blanket", "Pillow", "Mattress",
+	"Toaster", "Vacuum", "Kettle", "Fridge", "Blender",
+	"Teapot", "Cupcake", "Nugget", "Cracker", "Marshmallow",
+	"Jellybean", "Lollipop", "Caramel", "Gummy", "Sprinkle",
+	"Peanut", "Walnut", "Pistachio", "Cashew", "Almond",
+	"Thunderbolt", "Firefly", "Sunflower", "Dandelion", "Mushroom",
+	"Pebble", "Boulder", "Cactus", "Coconut", "Pineapple",
+	"Avocado", "Broccoli", "Carrot", "Pickle", "Pumpkin",
+	"Turnip", "Radish", "Corn", "Bean", "Potato",
 }
 
 // --- Dutch -----------------------------------------------------------
 
-var nlAdjectives = []string{
-	"Rode", "Blauwe", "Groene", "Oranje", "Roze", "Gele", "Grijze",
-	"Bruine", "Zwarte", "Paarse",
-	"Boze", "Knappe", "Krokante",
-	"Smerige", "Snelle", "Trage", "Natte", "Droge", "Harige", "Kale",
-}
-
-var nlNouns = []string{
+var nlWords = []string{
 	"Banaan", "Aardappel", "Pannenkoek", "Snotneus", "Aardbei",
 	"Kaas", "Boterham", "Knakworst", "Kipnugget", "Schoen",
 	"Frikandel", "Stofzuiger", "Badeend", "Tosti", "Kroket",
-	"Fietsbel", "Sok",
+	"Fietsbel", "Sok", "Pannenkoek", "Koekje", "Wafel",
+	"Appel", "Peer", "Kers", "Druif", "Meloen",
+	"Ananas", "Kokosnoot", "Avocado", "Wortel", "Komkommer",
+	"Pompoen", "Radijs", "Champignon", "Broccoli", "Maïs",
+	"Penguin", "Aap", "Giraf", "Olifant", "Otter",
+	"Das", "Panda", "Koala", "Hamster", "Egel",
+	"Draak", "Tovenaar", "Kabouter", "Piraat", "Ninja",
+	"Robot", "Alien", "Monster", "Spook", "Vampier",
+	"Bliksem", "Regenboog", "Komeet", "Meteoor", "Zonneschijn",
+	"Bel", "Emmer", "Hamer", "Potlood", "Rugzak",
+	"Deken", "Kussen", "Matras", "Broodrooster", "Waterkoker",
+	"Koelkast", "Blender", "Theepot", "Cupcake", "Nugget",
+	"Knackworst", "Dropje", "Spekje", "Lolly", "Kauwgom",
+	"Pinda", "Walnoot", "Pistache", "Cashew", "Amandel",
+	"Vuurvlieg", "Zonnebloem", "Paardenbloem", "Paddenstoel", "Kiezel",
+	"Rots", "Cactus", "Sinaasappel", "Mandarijn", "Citroen",
+	"Tomaat", "Ui", "Knoflook", "Boon", "Mais",
 }
 
-var wordLists = map[i18n.Locale]struct{ adjectives, nouns []string }{
-	i18n.EN: {enAdjectives, enNouns},
-	i18n.NL: {nlAdjectives, nlNouns},
+var wordLists = map[i18n.Locale][]string{
+	i18n.EN: enWords,
+	i18n.NL: nlWords,
 }
 
 func generateUsername(locale i18n.Locale) string {
 	words := wordLists[locale]
 
-	return fmt.Sprintf("%s%s",
-		words.adjectives[rand.IntN(len(words.adjectives))],
-		words.nouns[rand.IntN(len(words.nouns))],
-	)
+	return words[rand.IntN(len(words))]
 }
--- src/playhaus-app/src/features/settings/profile.ts
+++ src/playhaus-app/src/features/settings/profile.ts
@@ -20,21 +20,57 @@ export const SETTINGS: Setting[] = [
     { key: 'enableVibration', icon: 'smartphone', titleKey: 'profile.settings.vibration.title', descriptionKey: 'profile.settings.vibration.description' }
 ];
 
-const RANDOM_NAME_PARTS: Record<LanguageCode, { adjectives: string[], nouns: string[] }> = {
-    nl: {
-        adjectives: ['Rode', 'Blauwe', 'Groene', 'Oranje', 'Roze', 'Gele', 'Grijze', 'Bruine', 'Zwarte'],
-        nouns: ['Banaan', 'Aardappel', 'Pannenkoek', 'Snotneus', 'Drol', 'Kaas', 'Boterham', 'Knakworst', 'Kipnugget', 'Schoen']
-    },
-    en: {
-        adjectives: ['Red', 'Blue', 'Green', 'Orange', 'Pink', 'Yellow', 'Gray', 'Orange', 'Brown', 'Black'],
-        nouns: ['Banana', 'Potato', 'Pancake', 'Goofball', 'Dumpling', 'Cheese', 'Noodle', 'Meatball', 'Pickle', 'Shoe']
-    }
+const RANDOM_NAME_WORDS: Record<LanguageCode, string[]> = {
+    nl: [
+        'Banaan', 'Aardappel', 'Pannenkoek', 'Snotneus', 'Drol',
+        'Kaas', 'Boterham', 'Knakworst', 'Kipnugget', 'Schoen',
+        'Frikandel', 'Stofzuiger', 'Badeend', 'Tosti', 'Kroket',
+        'Fietsbel', 'Sok', 'Koekje', 'Wafel', 'Appel',
+        'Peer', 'Kers', 'Druif', 'Meloen', 'Ananas',
+        'Kokosnoot', 'Avocado', 'Wortel', 'Komkommer', 'Pompoen',
+        'Radijs', 'Champignon', 'Broccoli', 'Mais', 'Penguin',
+        'Aap', 'Giraf', 'Olifant', 'Otter', 'Das',
+        'Panda', 'Koala', 'Hamster', 'Egel', 'Draak',
+        'Tovenaar', 'Kabouter', 'Piraat', 'Ninja', 'Robot',
+        'Alien', 'Monster', 'Spook', 'Vampier', 'Bliksem',
+        'Regenboog', 'Komeet', 'Meteoor', 'Zonneschijn', 'Vuurvlieg',
+        'Zonnebloem', 'Paardenbloem', 'Paddenstoel', 'Kiezel', 'Rots',
+        'Cactus', 'Sinaasappel', 'Mandarijn', 'Citroen', 'Tomaat',
+        'Ui', 'Knoflook', 'Boon', 'Emmer', 'Hamer',
+        'Potlood', 'Rugzak', 'Deken', 'Kussen', 'Matras',
+        'Broodrooster', 'Waterkoker', 'Koelkast', 'Blender', 'Theepot',
+        'Cupcake', 'Nugget', 'Dropje', 'Spekje', 'Lolly',
+        'Kauwgom', 'Pinda', 'Walnoot', 'Pistache', 'Cashew',
+        'Amandel', 'Knuffel', 'Boterkoek', 'Koffiekop', 'Paraplu',
+    ],
+
+    en: [
+        'Banana', 'Potato', 'Pancake', 'Goofball', 'Dumpling',
+        'Cheese', 'Noodle', 'Meatball', 'Pickle', 'Shoe',
+        'Frisbee', 'Biscuit', 'Waffle', 'Muffin', 'Cookie',
+        'Donut', 'Pretzel', 'Popcorn', 'Taco', 'Burrito',
+        'Penguin', 'Monkey', 'Giraffe', 'Elephant', 'Otter',
+        'Badger', 'Panda', 'Koala', 'Hamster', 'Hedgehog',
+        'Dragon', 'Wizard', 'Goblin', 'Pirate', 'Ninja',
+        'Robot', 'Alien', 'Monster', 'Ghost', 'Vampire',
+        'Thunder', 'Rainbow', 'Comet', 'Meteor', 'Sunshine',
+        'Firefly', 'Sunflower', 'Dandelion', 'Mushroom', 'Pebble',
+        'Boulder', 'Cactus', 'Coconut', 'Pineapple', 'Avocado',
+        'Carrot', 'Cucumber', 'Pumpkin', 'Radish', 'Broccoli',
+        'Tomato', 'Onion', 'Garlic', 'Bean', 'Bucket',
+        'Hammer', 'Pencil', 'Backpack', 'Blanket', 'Pillow',
+        'Mattress', 'Toaster', 'Kettle', 'Fridge', 'Blender',
+        'Teapot', 'Cupcake', 'Nugget', 'Marshmallow', 'Lollipop',
+        'Gummy', 'Peanut', 'Walnut', 'Pistachio', 'Cashew',
+        'Almond', 'Button', 'Slipper', 'Socks', 'Teacup',
+        'Umbrella', 'Whistle', 'Broom', 'Bubble', 'Sparkle',
+        'Rocket', 'Spaceship', 'Treasure', 'Jellybean', 'Muffin',
+    ],
 };
 
 export function randomName(language: LanguageCode): string {
-    const parts = RANDOM_NAME_PARTS[language];
+    const words = RANDOM_NAME_WORDS[language];
     const pick = <T,>(items: T[]) => items[Math.floor(Math.random() * items.length)];
 
-    return `${pick(parts.adjectives)}${pick(parts.nouns)}`
-        .slice(0, NAME_MAX_LENGTH);
+    return pick(words).slice(0, NAME_MAX_LENGTH);
 }
